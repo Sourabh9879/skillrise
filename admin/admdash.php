4dd80c09f42e71d1bce614ba9017e799e95c451e
Sourabh9879 <kumarsourabh970@gmail.com>
2025-01-03 10:15:25 +0530

    home linnk  add

--- admin/admdash.php
+++ admin/admdash.php
@@ -69,6 +69,11 @@
                 <div class="sidebar-sticky">
                     <ul class="nav flex-column">
                         <li class="nav-item">
+                            <a class="nav-link active" href="../index.php">
+                                Home
+                            </a>
+                        </li>
+                        <li class="nav-item">
                             <a class="nav-link active" href="admdash.php">
                                 Dashboard
                             </a>
